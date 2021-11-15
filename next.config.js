58dc46caf8ecb038dc27bb57e65108a5cfe923f2
Jeve-Stobs <samuelacasey@gmail.com>
2021-11-14 20:00:46 -0500

    add redirects

--- next.config.js
+++ next.config.js
@@ -2,5 +2,45 @@ module.exports = {
   generateBuildId: () => 'build',
   distDir: 'build',
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/invite',
+        destination:
+          'https://discord.com/api/oauth2/authorize?client_id=816842865732091924&permissions=8&redirect_uri=http%3A%2F%2Foauth.acoustic.to&response_type=code&scope=bot%20guilds.join%20identify%20applications.commands',
+        permanent: true
+      },
+      {
+        source: '/support',
+        destination: 'https://discord.gg/8gspb92Cns',
+        permanent: true
+      },
+      {
+        source: '/vote',
+        destination: 'https://top.gg/bot/816842865732091924',
+        permanent: true
+      },
+      {
+        source: '/servervote',
+        destination: 'https://top.gg/servers/830229630950047774',
+        permanent: true
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/acousticlybot',
+        permanent: true
+      },
+      {
+        source: '/youtube',
+        destination: 'https://www.youtube.com/channel/UCukKSll51JBQ1t1V9MVRHeA',
+        permanent: true
+      },
+      {
+        source: '/patreon',
+        destination: 'https://www.patreon.com/acousticly',
+        permanent: true
+      }
+    ]
+  }
 }
