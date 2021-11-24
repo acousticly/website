5f11a6141c5748f9297cdd53c4fc30871a5e2019
Jeve-Stobs <samuelacasey@gmail.com>
2021-11-23 19:19:35 -0500

    add user scalability

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function App({ Component, pageProps }) {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
         <title>Acoustic</title>
         <link rel="preconnect" href="https://discord.com" />
