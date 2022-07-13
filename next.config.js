164bf447dc1af4a36a25c83275fdd8d16690ddcb
Jeve-Stobs <sam@jevestobs.dev>
2022-07-13 01:46:37 -0400

    🐛 fix images

--- next.config.js
+++ next.config.js
@@ -9,13 +9,6 @@ module.exports = withPreact({
     optimizeCss: true,
     scrollRestoration: true
   },
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    iconSizes: [],
-    domains: ['https://res.cloudinary.com/'],
-    path: '',
-    loader: 'cloudinary'
-  },
   async redirects() {
     return [
       {
